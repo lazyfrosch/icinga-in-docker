8bdaa0024f42da738594d7940106e26c8458fe32
Markus Frosch <markus@lazyfrosch.de>
2020-04-06 17:28:40 +0200

    git init

---
+++ README.md
@@ -0,0 +1,6 @@
+Icinga in Docker
+================
+
+This is an **example** how to use Icinga 2 with Icinga Web 2 and IcingaDB inside Docker containers.
+
+**WARNING:** This is not meant to and not supported for production, I'm using this for my test setups.
---
+++ mysql-databases.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE icingaweb2;
+GRANT ALL ON icingaweb2.* TO icinga@'%';
+
+CREATE DATABASE icinga2;
+GRANT ALL ON icinga2.* TO icinga@'%';
+
+CREATE DATABASE icingadb;
+GRANT ALL ON icingadb.* TO icinga@'%';
+
+CREATE DATABASE icinga_director CHARACTER SET 'utf8';
+GRANT ALL ON icinga_director.* TO icinga@'%';
